9ce1d3b3112ac02a257cfe761e4153daa30eaeda
Friday (Build Bot) <friday@birchstreetlaw.build>
2026-03-25 22:23:46 -0400

    fix: Move global CSS link out of Svelte script block to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,7 @@
   Root layout: imports global CSS, wraps every page with Nav + Footer.
 -->
 <script>
-<link rel="stylesheet" href="%sveltekit.assets%/global.css" media="print" onload="this.media='all'" />
-  <noscript><link rel="stylesheet" href="%sveltekit.assets%/global.css" /></noscript>
+
   import Nav from '../lib/Nav.svelte';
   import Footer from '../lib/Footer.svelte';
   import { afterNavigate } from '$app/navigation';
